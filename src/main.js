e63d2cecdfa6f67982f64100f6105dc45408fca5
JoshuaRobertson <josh.robertson@coinclear.uk>
2022-11-07 13:59:57 +0000

    chore: disable package warnings

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import axios from 'axios'
 
 Vue.config.productionTip = false
-
 Vue.prototype.$axios = axios
+Vue.config.silent = true
 
 new Vue({
   render: h => h(App),
